d79db254573f1dd2f97c59d8ce6f3c5a0cf11715
Muhammed Ali Siddiqui <alisiddiqui1997@hotmail.com>
2020-07-13 02:33:44 -0700

    Models created

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50,null=True )
+    phone = models.CharField(max_length=50,null=True)
+    email = models.CharField(max_length=20,null=True)
+    date_created = models.DateTimeField(auto_now_add=True ,null=True)
+    profile_pic = models.ImageField(null=True,default='index.png')
+    admin = models.CharField(max_length=2,null=True,default='0')
+    
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,null=True )
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY =(
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door'),
+
+    )
+    name = models.CharField(max_length=20,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=20,null=True,choices=CATEGORY)
+    date_created = models.DateTimeField(auto_now_add=True ,null=True)
+    desciption = models.CharField(max_length=100,null=True,blank=True)
+    tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Out for delivery', 'Out for delivery' ),
+        ('Delivered','Delivered'),
+    )
+    
+    customer = models.ForeignKey(Customer,null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True ,null=True)
+    status = models.CharField(max_length=40,null=True,choices=STATUS)
+    def __str__(self):
+        return self.product.name
+    
